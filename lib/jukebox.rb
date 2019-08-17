d113a714de94675695e25d03bcaac09acd2fbc36
Benny Louie <bennylouie1412@gmail.com>
2019-08-17 23:37:20 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,11 +36,6 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   song_name = gets.strip
-  songs.each do |song|
-    if song.include(song_name)
-      puts "Playing #{song}"
-    end
-  end
   if songs.include?(song_name)
     puts "Playing #{song_name}"
     elsif !songs.include?(song_name)
