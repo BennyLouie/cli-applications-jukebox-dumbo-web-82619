4b42e81590fc9b996d2e6cceca19948ac6699277
Benny Louie <bennylouie1412@gmail.com>
2019-08-18 00:21:12 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -59,7 +59,7 @@ def run
   puts "Please enter a command:"
   user_input = gets.strip
   if user_input == "exit"
-    return exit_jukebox
+    exit_jukebox
   end
   if user_input != "exit"
     run
