0ea5417d9640ace2c1c040c9ac3f14986b9cef9e
Benny Louie <bennylouie1412@gmail.com>
2019-08-18 00:09:45 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -60,15 +60,16 @@ def run
   user_input = gets.strip
   if user_input == "exit"
     exit_jukebox
-    elsif user_input == "help"
-    help
-    run
-    elsif user_input == "list"
-    list(songs)
-    run
-    elsif user_input == "play"
-    play(songs)
-    run
+    if user_input != "exit"
+      run
+      if user_input == "help"
+        help
+        elsif user_input == "list"
+        list(songs)
+        elsif user_input == "play"
+        play(songs)
+      end
+    end
   end
 end
 
